3ed8c64ff90b959d42c6e34cfdbf130f055068dd
Alper Dağlı <alper379@gmail.com>
2019-01-28 13:35:09 +0300

    Add project files.

---
+++ 01-Layout/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _01_Layout.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+    }
+}
---
+++ 01-Layout/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_01_Layout.MvcApplication" Language="C#" %>
